2aa5ca95f9a6887750bea020932e5a369611280e
jcgoncalves1803-art <jcgoncalves1803@gmail.com>
2026-07-17 12:52:32 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SMARTSHEET_TOKEN = os.getenv("SMARTSHEET_API_TOKEN")
-SMARTSHEET_SHEET_ID = os.getenv("SMARTSHEET_SHEET_ID")
-SECRET_KEY = os.getenv("APP_SECRET_KEY")
+SMARTSHEET_TOKEN = "Rwrx12lpcpOPM0rauIyLf4k5izmwQmjsT8cc6"
+SMARTSHEET_SHEET_ID = "3103817011777412"
+SECRET_KEY = os.getenv("APP_SECRET_KEY", "chave_padrao_facilities")
 
 IBGE_API_BASE = "https://servicodados.ibge.gov.br/api/v1"
+
+PAINEL_SENHA = "BRMT_Facilities26"
